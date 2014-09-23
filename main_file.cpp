60d10395fe94d35e4e718b65ae5f6df38cb6fbdf
Gustu <aproszeciebardzo@gmail.com>
2014-09-23 22:31:41 +0200

    added q and e keys

--- main_file.cpp
+++ main_file.cpp
@@ -44,7 +44,7 @@ float horizontalAngle = 3.14f;
 float verticalAngle = 0.0f;
 // Initial Field of View
 float FoV = 60.0f;
- 
+
 float speed = 1.0f; // 3 units / second
 float mouseSpeed = 0.005f;
 float zoomSpeed = 3.0f;
@@ -77,11 +77,6 @@ void ballsCreate();
 void surfaceCreate();
 void createOneBall();
 
-
-float terrainFunc(float x, float y){
-	return x*x/200 + y*y/200;
-}
-
 DisplaySettings display = {800, 400};
 
 dWorldID world;
@@ -89,34 +84,26 @@ dSpaceID space;
 dJointGroupID cgroup;
 
 bool mouseButton[] = {0,0,0,0};
-bool keyPressed[] = {0,0,0,0};
+bool keyPressed[] = {0,0,0,0,0,0};
 
 mat4 v,p;
 
 vector<Object> objects;
 
 int object_count = 0;
-//keyboard handlers
-void keyDownHandler(int c, int x, int y){
 
-}
-
-void keyUpHandler(int c, int x, int y){
-
-}
-
-void MouseRoutine(int x, int y){	
+void MouseRoutine(int x, int y){
 	if(mouseButton[0] == 1) {
 		xpos = x;
 		ypos = y;
 	}
 }
 
-void mouseButtonFunc(int button, int state, int x, int y) {	
+void mouseButtonFunc(int button, int state, int x, int y) {
 	if (button == 0 && state != GLUT_UP) {
-		mouseButton[0] = 1;	
+		mouseButton[0] = 1;
 	} else if(mouseButton[0] == 1 && state == GLUT_UP && button == 0) {
-		mouseButton[0] = 0;	
+		mouseButton[0] = 0;
 		xpos = x;
 		ypos = y;
 	}
@@ -127,7 +114,7 @@ void mouseButtonFunc(int button, int state, int x, int y) {
 		mouseButton[1] = 0;
 	}
 	if (button == 3 && state != GLUT_UP) {
-		if(FoV > 30.0f)FoV-=zoomSpeed;	
+		if(FoV > 30.0f)FoV-=zoomSpeed;
 	} else if(button == 4 && state != GLUT_UP) {
 		if(FoV < 120.0f)FoV+=zoomSpeed;
 	}
@@ -145,18 +132,19 @@ void createOneBall(){
 	dGeomSetBody(ball.geom, ball.body);
 	dGeomSetData(ball.geom, (void*)"ball");
 
+	// Direction : Spherical coordinates to Cartesian coordinates conversion
 	glm::vec3 direction(
-    	cos(verticalAngle) * sin(horizontalAngle),
-    	sin(verticalAngle),
-    	cos(verticalAngle) * cos(horizontalAngle)
-	);
+		cos(verticalAngle) * sin(horizontalAngle),
+		sin(verticalAngle),
+		cos(verticalAngle) * cos(horizontalAngle)
+		);
 
 	dGeomSetPosition(ball.geom, position.x + distanceFromCamera * direction.x, position.y + distanceFromCamera * direction.y, position.z + distanceFromCamera * direction.z);
 
 	ball.color[0] = rand()%1000 * 0.001;
 	ball.color[1] = rand()%1000 * 0.001;
 	ball.color[2] = rand()%1000 * 0.001;
-	ball.color[3] = 1.0f;	
+	ball.color[3] = 1.0f;
 
 	dBodyAddForce(ball.body, force*direction.x, force*direction.y, force*direction.z);
 
@@ -165,32 +153,39 @@ void createOneBall(){
 }
 
 void keyDownHandler(unsigned char c, int x, int y){
-    if(c == 'w') {
-    	keyPressed[0] = 1;
-    } else if(c == 'a') {
-    	keyPressed[1] = 1;
-    } else if(c == 's') {
-    	keyPressed[2] = 1;
-    } else if(c == 'd') {
-    	keyPressed[3] = 1;
-    } else if(c == 27) {
-    	dWorldDestroy(world);
-  		dCloseODE();
-    	exit(0);
-    }
-
+	if(c == 'w') {
+		keyPressed[0] = 1;
+	} else if(c == 'a') {
+		keyPressed[1] = 1;
+	} else if(c == 's') {
+		keyPressed[2] = 1;
+	} else if(c == 'd') {
+		keyPressed[3] = 1;
+	} else if(c == 'q') {
+		keyPressed[4] = 1;
+	} else if(c == 'e') {
+		keyPressed[5] = 1;
+	} else if(c == 27) {
+		dWorldDestroy(world);
+		dCloseODE();
+		exit(0);
+	}
 }
 
 void keyUpHandler(unsigned char c, int x, int y){
 	if(c == 'w') {
-    	keyPressed[0] = 0;
-    } else if(c == 'a') {
-    	keyPressed[1] = 0;
-    } else if(c == 's') {
-    	keyPressed[2] = 0;
-    } else if(c == 'd') {
-    	keyPressed[3] = 0;
-    }
+		keyPressed[0] = 0;
+	} else if(c == 'a') {
+		keyPressed[1] = 0;
+	} else if(c == 's') {
+		keyPressed[2] = 0;
+	} else if(c == 'd') {
+		keyPressed[3] = 0;
+	} else if(c == 'q') {
+		keyPressed[4] = 0;
+	} else if(c == 'e') {
+		keyPressed[5] = 0;
+	}
 }
 //*************
 static void nearCallback(void *data, dGeomID o1, dGeomID o2) {
@@ -222,42 +217,47 @@ void Draw() {
 	if (mouseButton[0]) {
 		horizontalAngle += mouseSpeed * deltaTime/500.0f * float(display.width/2 - xpos );
 		verticalAngle   += mouseSpeed * deltaTime/500.0f * float(display.height/2 - ypos );
-	}	
+	}
 
+	// Direction : Spherical coordinates to Cartesian coordinates conversion
 	glm::vec3 direction(
-    	cos(verticalAngle) * sin(horizontalAngle),
-    	sin(verticalAngle),
-    	cos(verticalAngle) * cos(horizontalAngle)
-	);
+		cos(verticalAngle) * sin(horizontalAngle),
+		sin(verticalAngle),
+		cos(verticalAngle) * cos(horizontalAngle)
+		);
 
 	glm::vec3 right = glm::vec3(
-	    sin(horizontalAngle - 3.14f/2.0f),
-	    0,
-	    cos(horizontalAngle - 3.14f/2.0f)
-	);
-
-	if(keyPressed[0]) {
-		position += direction * (deltaTime/1.0f) * speed; 
-    } else if(keyPressed[1]) {
-    	position -= right * (deltaTime/1.0f) * speed;
-    } else if(keyPressed[2]) {
-    	position -= direction * (deltaTime/1.0f) * speed;
-    } else if(keyPressed[3]) {
-    	position +=    right * (deltaTime/1.0f) * speed;
-    }
+		sin(horizontalAngle - 3.14f/2.0f),
+		0,
+		cos(horizontalAngle - 3.14f/2.0f)
+		);
 
 	// Up vector : perpendicular to both direction and right
 	glm::vec3 up = glm::cross( right, direction );
 
+	if(keyPressed[0]) {
+		position += direction * (deltaTime/1.0f) * speed;
+	} else if(keyPressed[1]) {
+		position -= right * (deltaTime/1.0f) * speed;
+	} else if(keyPressed[2]) {
+		position -= direction * (deltaTime/1.0f) * speed;
+	} else if(keyPressed[3]) {
+		position +=    right * (deltaTime/1.0f) * speed;
+	} else if(keyPressed[4]) {
+		position +=    up * (deltaTime/1.0f) * speed;
+	} else if(keyPressed[5]) {
+		position -=    up * (deltaTime/1.0f) * speed;
+	}
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();//???????
-	
+
 	p = perspective(FoV, 1.0f * display.width / display.height , 40.0f, 5000.0f);
 
 	v = lookAt( position,// y is a height
-										position + direction,
-										up);
-	
+		position + direction,
+		up);
+
 	glLoadMatrixf(value_ptr(p));
 	glMatrixMode(GL_MODELVIEW);
 	glLoadMatrixf(value_ptr(v));
@@ -267,52 +267,50 @@ void Draw() {
 	dWorldQuickStep(world, 0.05);//much better with step = 0.02 als 0.1
 
 	dJointGroupEmpty(cgroup);
-	
+
 	glClearColor(0.0f, 0.3984375f, 0.796875f, 0.0f);
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-	
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
+
 	int c = 0;
 
-	  for (int i=0; i<object_count - c; i++) {
-	  	const dReal *realP = dBodyGetPosition(objects[i].body);
-	  	if(objects[i].type!=PLANE && sqrt(realP[0]*realP[0] + realP[1]*realP[1] + realP[2]*realP[2]) > disaperePoint){
-	  		printf("Deleted object %d at %f %f %f\n", objects[i].type, realP[0], realP[1], realP[2]);
-	  		objects.erase(objects.begin() + i);  		
-	  		c++;
-	  	}
-	  	else
-	  		DrawObject(objects[i]);
-	  }
+	for (int i=0; i<object_count - c; i++) {
+		const dReal *realP = dBodyGetPosition(objects[i].body);
+		if(objects[i].type!=PLANE && sqrt(realP[0]*realP[0] + realP[1]*realP[1] + realP[2]*realP[2]) > disaperePoint){
+			printf("Deleted object %d at %f %f %f\n", objects[i].type, realP[0], realP[1], realP[2]);
+			objects.erase(objects.begin() + i);
+			c++;
+		}
+		else
+			DrawObject(objects[i]);
+	}
 
-	  object_count -= c;
-	  glFlush();
-	  glutSwapBuffers();
+	object_count -= c;
+	glFlush();
+	glutSwapBuffers();
 }
 
 void Initialize() {
-	glEnable(GL_NORMALIZE);
-
 	glClearDepth(1.0f);
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_NORMALIZE);
-	
+
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 	glShadeModel(GL_SMOOTH);
-	
+
 	GLfloat qaAmbientLight[]	= {1.0, 1.0, 1.0, 1.0};
 	GLfloat qaDiffuseLight[]	= {1.0, 1.0, 1.0, 1.0};
 	GLfloat qaSpecularLight[]	= {1.0, 1.0, 1.0, 1.0};
 	glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);	
-	
+	glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
+
 	GLfloat qaLightPosition[]	= {100.0, 500.0, 100.0, 0.0};
 	glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
 	glEnable(GL_LIGHT0);
@@ -343,33 +341,33 @@ void Initialize() {
 
 }
 void Timer(int iUnused) {
-    glutPostRedisplay();
+	glutPostRedisplay();
     glutTimerFunc(1, Timer, 0);//??
 }
 
 /*****************/
 void DrawObject(Object& ob){
 	// materials setup per object basis
-	
-	
+
+
 	if (ob.type == BALL){
 		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ob.color);
 		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, ob.color);
 		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, ob.color);
 		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, ob.color);
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50.0f);
-		
+
 		//glMaterialf(GL_FRONT, GL_SHININESS, 120.0);
-		
+
 		const dReal *realP = dBodyGetPosition(ob.body);
 
 		glMatrixMode(GL_MODELVIEW);
 		glLoadMatrixf(value_ptr( v
-		 * translate(mat4(1.0f), vec3(realP[0], realP[1], realP[2]))
-		));
+			* translate(mat4(1.0f), vec3(realP[0], realP[1], realP[2]))
+			));
 
 		glutSolidSphere(dGeomSphereGetRadius(ob.geom), 40, 20);
-		
+
 
 	} else{
 		//The color of the sphere
@@ -381,7 +379,7 @@ void DrawObject(Object& ob){
 		//The color emitted by the material
 		GLfloat materialEmission[] = {0.0f,0.0f,0, 1.0f};
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0.0f);
-	
+
 
 		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
 		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, materialSpecular);
@@ -391,14 +389,14 @@ void DrawObject(Object& ob){
 		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 		glShadeModel(GL_FLAT);
 		glMatrixMode(GL_MODELVIEW);
-		glLoadMatrixf(value_ptr(v ));//*translate(mat4(1.0f),vec3(-200,-200,-500))* scale(mat4(1.0f), vec3(100.0f,10.0f,100.0f))));
+		glLoadMatrixf(value_ptr(v ));
 		glBindTexture(GL_TEXTURE_2D,tex);
-	
+
 		glEnableClientState(GL_VERTEX_ARRAY);
 		//glEnableClientState(GL_COLOR_ARRAY);
 		glEnableClientState(GL_NORMAL_ARRAY);
 		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-	
+
 		glVertexPointer(3, GL_FLOAT, 0, Vertices);
 
 		glTexCoordPointer( 2, GL_FLOAT, 0, geomTexCoords);
@@ -406,7 +404,7 @@ void DrawObject(Object& ob){
 		//glColorPointer(3, GL_FLOAT, 0, Colors);
 		//glDrawArrays(GL_LINES, 0, VertexCount);
 		glDrawElements(GL_TRIANGLES, IndexCount, GL_UNSIGNED_INT, gl_Indices);
-	
+
 		glDisableClientState(GL_VERTEX_ARRAY);
 		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 		//glDisableClientState(GL_COLOR_ARRAY);
@@ -418,39 +416,39 @@ void DrawObject(Object& ob){
 
 void ballsCreate(){
 	for (int i=0;i<OBJ_COUNT;i++) {
-			Object ball;
-			ball.body = dBodyCreate(world);
-			ball.type = BALL;
-			dMassSetZero(&ball.mass);
-			dMassSetSphereTotal(&ball.mass, 1, 10);
-			dBodySetMass(ball.body, &ball.mass);
+		Object ball;
+		ball.body = dBodyCreate(world);
+		ball.type = BALL;
+		dMassSetZero(&ball.mass);
+		dMassSetSphereTotal(&ball.mass, 1, 10);
+		dBodySetMass(ball.body, &ball.mass);
 
-			ball.geom = dCreateSphere(space, 10);
-			dGeomSetBody(ball.geom, ball.body);
-			dGeomSetData(ball.geom, (void*)"ball");
-			dGeomSetPosition(ball.geom, rand()%400 -200.0f, 200.0 +i * 30.0, rand() % 200 - 100.0f);
+		ball.geom = dCreateSphere(space, 10);
+		dGeomSetBody(ball.geom, ball.body);
+		dGeomSetData(ball.geom, (void*)"ball");
+		dGeomSetPosition(ball.geom, rand()%400 -200.0f, 200.0 +i * 30.0, rand() % 200 - 100.0f);
 
-			ball.color[0] = rand()%1000 * 0.001;
-			ball.color[1] = rand()%1000 * 0.001;
-			ball.color[2] = rand()%1000 * 0.001;
-			ball.color[3] = 1.0f;
+		ball.color[0] = rand()%1000 * 0.001;
+		ball.color[1] = rand()%1000 * 0.001;
+		ball.color[2] = rand()%1000 * 0.001;
+		ball.color[3] = 1.0f;
 
-			objects.push_back(ball);
-		}
+		objects.push_back(ball);
+	}
 		object_count = OBJ_COUNT;//to change
-}
-void surfaceCreate(){//to do: change plane to a custom surface 
+	}
+void surfaceCreate(){//to do: change plane to a custom surface
 	Object plane;
 	plane.type = PLANE;
-	
+
 	plane.color[0]=0.6;
 	plane.color[1]=0.3;
 	plane.color[2]=0.0;
 	plane.color[3]=1;
 
 	dTriMeshDataID new_tmdata = dGeomTriMeshDataCreate();
-	dGeomTriMeshDataBuildSingle(new_tmdata, &Vertices[0], 3 * sizeof(float), VertexCount, 
-				&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
+	dGeomTriMeshDataBuildSingle(new_tmdata, &Vertices[0], 3 * sizeof(float), VertexCount,
+		&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
 
 
 	plane.geom = dCreateTriMesh(space, new_tmdata, 0, 0, 0);
@@ -460,37 +458,35 @@ void surfaceCreate(){//to do: change plane to a custom surface
 
 dWorldID worldInit(){
 	world = dWorldCreate();
-  dWorldSetGravity(world, 0.0, -9.81, 0.0);
-  dWorldSetERP(world, 0.9);
-  dWorldSetCFM(world, 1e-4);
-  dWorldSetLinearDamping(world, 0.00001);
-  dWorldSetAngularDamping(world, 0.005);
-  dWorldSetMaxAngularSpeed(world, 200);
-  dWorldSetContactMaxCorrectingVel(world, 0.1);
-  dWorldSetContactSurfaceLayer(world, 0.1);
-  
-  return world;
+	dWorldSetGravity(world, 0.0, -9.81, 0.0);
+	dWorldSetERP(world, 0.9);
+	dWorldSetCFM(world, 1e-4);
+	dWorldSetLinearDamping(world, 0.00001);
+	dWorldSetAngularDamping(world, 0.005);
+	dWorldSetMaxAngularSpeed(world, 200);
+	dWorldSetContactMaxCorrectingVel(world, 0.1);
+	dWorldSetContactSurfaceLayer(world, 0.001);
+
+	return world;
 }
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB | GLUT_DOUBLE);
-  glutInitWindowSize(display.width, display.height);
-  glutInitWindowPosition(200, 200);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(display.width, display.height);
+	glutInitWindowPosition(200, 200);
 
-  glutCreateWindow("OpenGL2.0 + ODE: Balls Physics");
-  Initialize();
-  dInitODE();
+	glutCreateWindow("OpenGL2.0 + ODE: Balls Physics");
+	Initialize();
+	dInitODE();
 
-  world = worldInit();
+	world = worldInit();
 
-  space = dSimpleSpaceCreate(0);
-  cgroup = dJointGroupCreate(0);
-  
+	space = dSimpleSpaceCreate(0);
+	cgroup = dJointGroupCreate(0);
+
   //keyboard handlers
-	glutSpecialFunc(keyDownHandler);
-	glutSpecialUpFunc(keyUpHandler);
 	glutKeyboardFunc(keyDownHandler);
 	glutKeyboardUpFunc(keyUpHandler);
 
@@ -498,19 +494,19 @@ int main(int argc, char** argv) {
 	glutWarpPointer(display.width/2,display.height/2);
 	glutMouseFunc(mouseButtonFunc);
 
-  
-  // make some balls
-  ballsCreate();
-  //Create a surface
-  surfaceCreate();
-  
-  glutDisplayFunc(Draw);
-  Timer(0);
-  
-  glutMainLoop();
-  dWorldDestroy(world);
 
-  dCloseODE();
-  glDeleteTextures(1,&tex);
-  return 0;
+  // make some balls
+	ballsCreate();
+  //Create a surface
+	surfaceCreate();
+
+	glutDisplayFunc(Draw);
+	Timer(0);
+
+	glutMainLoop();
+	dWorldDestroy(world);
+
+	dCloseODE();
+	glDeleteTextures(1,&tex);
+	return 0;
 }
